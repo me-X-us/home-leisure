10352ab711a583c80d2717543af11307985443fd
남광운 <>
2020-09-15 12:40:39 +0900

    init: 초기화 커밋

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Webcam from "react-webcam";
+
+function App() {
+  return (
+    <div>
+      <Webcam/>
+    </div>
+  );
+}
+
+export default App;
