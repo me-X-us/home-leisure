96c95bb68ca5cb1b65e124d5975a2329f39299bf
남광운 <>
2020-09-23 14:16:46 +0900

    (미완성) 메인페이지 & 툴바 & 트레이닝페이지

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Webcam from "react-webcam";
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+import Home from './pages/Main';
+import Training from './pages/Training';
+import Mypage from './pages/Main';
 
 function App() {
   return (
-    <div>
-      <Webcam/>
+    <div className="Start">
+      <Router>
+        <div className='Menu-wrapper'>
+          <ul>
+            <Link to='/'><button>Home</button></Link>
+            <Link to='/mypage'><button>MyPage</button></Link>
+            <Link to='/training'><button>Training</button></Link>
+          </ul>
+          <ul>
+          </ul>
+        </div>
+        <div className='Contents-wrapper'>
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route path='/mypage' component={Mypage} />
+            <Route path='/training' component={Training} />
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
---
+++ src/pages/Training.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Webcam from 'react-webcam';
+import ReactPlayer from 'react-player';
+import Player from '../components/Player.js'
+
+function training() {
+  return (
+    <div>
+      <Player />
+    </div>
+  );
+}
+
+export default training;
