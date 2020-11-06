627847fe8ab9f33a5acdd6ab5e7d8981b807a13f
always0ne <si8363@soongsil.ac.kr>
2020-11-06 09:40:06 +0000

    Deploying to gh-pages from  @ 2db874ecd4ec9fcdb50a5d368eba26e5d0a08a77 🚀

--- precache-manifest.7e98673550f4fc3678e19083dbd2e78a.js
+++ precache-manifest.7e98673550f4fc3678e19083dbd2e78a.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "e2cdac6272f7ca4f2da618fafa8c615b",
+    "revision": "92d67ef3ba6b1666ac013fcef5d38820",
     "url": "/home-leisure/index.html"
   },
   {
-    "revision": "2ee3c111490d503dfd6a",
-    "url": "/home-leisure/static/css/main.10ebe4d6.chunk.css"
+    "revision": "eda6a2bd054476e941e6",
+    "url": "/home-leisure/static/css/main.7aae92a6.chunk.css"
   },
   {
     "revision": "eb99081efe62fca51eb5",
@@ -16,7 +16,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/home-leisure/static/js/2.568b7189.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "2ee3c111490d503dfd6a",
+    "revision": "eda6a2bd054476e941e6",
     "url": "/home-leisure/static/js/main.7fb5f3f7.chunk.js"
   },
   {
