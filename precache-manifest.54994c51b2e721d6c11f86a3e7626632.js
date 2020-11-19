1909a560e0d47039b390d50f9ea92f677bf79d73
Namkwangwoon <nkw096@naver.com>
2020-11-19 01:29:53 +0000

    Deploying to gh-pages from  @ 603656519fa9ff5cf9cb3a10c7ab5d4796c03b64 🚀

---
+++ precache-manifest.54994c51b2e721d6c11f86a3e7626632.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5d291dd0a508f11cf84d9f0ffecd953",
+    "url": "/home-leisure/index.html"
+  },
+  {
+    "revision": "91f2b77fe3d46b10c704",
+    "url": "/home-leisure/static/css/main.d498ef1a.chunk.css"
+  },
+  {
+    "revision": "90437426fb7ce0e2dbc6",
+    "url": "/home-leisure/static/js/2.04c784ea.chunk.js"
+  },
+  {
+    "revision": "13bb90a17078150ab8ab8e32e790efe4",
+    "url": "/home-leisure/static/js/2.04c784ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91f2b77fe3d46b10c704",
+    "url": "/home-leisure/static/js/main.75ee8b36.chunk.js"
+  },
+  {
+    "revision": "fdbb948745d089aef4e1",
+    "url": "/home-leisure/static/js/runtime-main.307bac84.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/home-leisure/precache-manifest.7e98673550f4fc3678e19083dbd2e78a.js"
+  "/home-leisure/precache-manifest.54994c51b2e721d6c11f86a3e7626632.js"
 );
 
 self.addEventListener('message', (event) => {
