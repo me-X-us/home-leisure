e2bced2408521d1c7e65948e575f9e0a427c0bb7
Namkwangwoon <nkw096@naver.com>
2020-11-22 02:13:45 +0900

    feat: Profile 이미지 미리보기 + 첫 로드시 기본 이미지

--- src/pages/ProfileOfTrainer.js
+++ src/pages/ProfileOfTrainer.js
@@ -1,17 +1,24 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import '../css/ProfileOfUser.css';
 import SearchVideoList from '../components/SearchVideoList';
-import {Link} from 'react-router-dom';
-import {getUserNickName} from '../utils/authHttpWrapper';
+import { Link } from 'react-router-dom';
+import { getUserId, getUserNickName, postHttp, API_BASE_URL } from '../utils/authHttpWrapper';
 
-const ProfileOfUser = (props) => {
+const ProfileOfTrainer = (props) => {
     const [nickName, setNickName] = useState('');
     const [nickNameChange, setNickNameChange] = useState(false);
     const [newNickName, setNewNickName] = useState('');
+    const imgDefault = 'https://avatars1.githubusercontent.com/u/50524321?s=400&u=7621eb647ffc21484a8ddb3914275574063c08cb&v=4';
+    const [imgPreview, setImagePreview] = useState('');
+    const [imgUpload, setImgUpload] = useState('');
+    const [imgChanging, setImageChanging] = useState(false);
 
     useEffect(() => {
-        getUserNickName().then( r => setNickName(r) )
-    },[])
+        getUserNickName()
+            .then(r => setNickName(r))
+            .then(() => getUserId())
+            .then(userId => setImgUpload(API_BASE_URL + '/profile/' + userId + '/image'))
+    }, [])
 
     const onChangeNickName = e => setNewNickName(e.target.value);
 
@@ -19,7 +26,9 @@ const ProfileOfUser = (props) => {
         // let nick = await getUserNickName();      // 수정
         if (nickNameChange === false) {
             setNickNameChange(true);
-        } else {
+        }
+        else if(nickNameChange === true && newNickName !== '') {
+
             setNickName(newNickName);
             setNickNameChange(false);
         }
@@ -31,54 +40,84 @@ const ProfileOfUser = (props) => {
         }
     }
 
+    const onImageChanged = (e) => {
+        setImagePreview(URL.createObjectURL(e.target.files[0]))
+        setImgUpload(e.target.files[0])
+    }
+
+    const submitImage = async (e) => {
+        if (imgChanging === false) {
+            setImageChanging(true);
+        }
+        else {
+            setImageChanging(false);
+
+            const fd = new FormData();
+            let userId = await getUserId();
+
+            fd.append('image', imgUpload);
+            postHttp('/profile/' + userId + '/image', fd)
+                .then(res => {
+                    setImgUpload(API_BASE_URL + '/profile/' + userId + '/image');
+                })
+                .catch(error => {
+                    console.log(error.response);
+                });
+        }
+    }
+
+    const onImageError = () => {
+        setImgUpload(imgDefault);
+    }
 
     return (
         <div>
-            <div style={{marginBottom: '200px', marginLeft: '200px'}}>
-                <img className='UserProfile '
-                     src="https://avatars1.githubusercontent.com/u/50524321?s=460&u=7621eb647ffc21484a8ddb3914275574063c08cb&v=4" alt={""}/>
-                <text style={{marginLeft: '400px'}}>
-                    <div className='UserInfo' style={{display: 'table-cell'}}>
+            <div style={{ height:'507px', marginBottom: '200px', marginLeft: '200px', float: 'left', display: 'flex', alignItems: 'flex-end', textAlign: 'center' }}>
+                <div>
+                    <img onError={onImageError} className='UserProfile' src={imgChanging?imgPreview:imgUpload}></img>
+                    {imgChanging ? <div><input type='file' accept='image/jpg,impge/png,image/jpeg,image/gif' onChange={onImageChanged} style={{width:'150px'}} alt={""} /></div>:<br/>}
+                    <button onClick={submitImage} style={{ marginTop: '10px', background: 'none', border: 'none' }}>프로필 편집</button>
+                </div>
+                <div style={{ marginLeft: '400px' }}>
+                    <div className='UserInfo' style={{ display: 'table-cell' }}>
                         <div className='NickName'>{nickNameChange ?
                             <input onChange={onChangeNickName} onKeyPress={onKeyPress}
-                                   placeholder={nickName}/> : nickName}</div>
+                                placeholder={nickName} /> : nickName}</div>
                         <button className='NickNameEditButton' onClick={changeNickName}>닉네임 편집</button>
                     </div>
-                </text>
+                </div>
             </div>
-            <div style={{marginLeft: '130px'}}>
-                <text>
-                    <div style={{display: 'table-cell', marginRight: '10px'}}>
-                        <text style={{fontSize: 'xx-large'}}>
-                            내가 올린 운동
+            <div style={{ marginLeft: '130px' }}>
+                <div style={{ display: 'table-cell', marginRight: '10px' }}>
+                    <text style={{ fontSize: 'xx-large' }}>
+                        내가 올린 운동
                         </text>
-                        <Link to='/upload'>
-                            <button style={{
-                                float: 'right',
-                                width: '42px',
-                                height: '42px',
-                                background: 'lightgray',
-                                paddingTop: '0px',
-                                border: 'none',
-                                borderRadius: '0.7rem',
-                                color: 'white',
-                                fontSize: 'xx-large',
-                                verticalAlign: 'center'
-                            }}>
-                                +
-                            </button>
-                        </Link>
-                        <div style={{
-                            backgroundColor: 'lightgray',
-                            width: '1500px',
-                            height: '800px',
-                            padding: '20px',
-                            marginTop: '10px'
+                    <Link to='/upload'>
+                        <button style={{
+                            float: 'right',
+                            width: '42px',
+                            height: '42px',
+                            background: 'lightgray',
+                            paddingTop: '0px',
+                            border: 'none',
+                            borderRadius: '0.7rem',
+                            color: 'white',
+                            fontSize: 'xx-large',
+                            verticalAlign: 'center'
                         }}>
-                            <SearchVideoList/>
-                        </div>
+                            +
+                            </button>
+                    </Link>
+                    <div style={{
+                        backgroundColor: 'lightgray',
+                        width: '1500px',
+                        height: '800px',
+                        padding: '20px',
+                        marginTop: '10px'
+                    }}>
+                        <SearchVideoList />
                     </div>
-                </text>
+                </div>
 
                 {/* <text style={{marginLeft:'20px'}}>
                     <div style={{ display: 'table-cell' }}>
@@ -95,4 +134,4 @@ const ProfileOfUser = (props) => {
     );
 }
 
-export default ProfileOfUser;
+export default ProfileOfTrainer;
