ff3de47f09c87aecdfc380389a0856cc213d745d
naemoo <gusdn0657@gmail.com>
2020-11-25 00:26:54 +0900

    refactor : mockData 제거

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,25 +1,44 @@
-import React, {useState} from 'react';
+import React, { useState, useEffect } from 'react';
 import '../css/Player.css';
 import ReactPlayer from 'react-player';
 import PoseNet from '../utils/posenet/components/PoseNet'
-import mockData from '../utils/mockData'
+//import mockData from '../utils/mockData'
 import cosineSimilarity from '../utils/cosineSimilarity'
+import { getHttp } from '../utils/authHttpWrapper';
 
-const fLen = mockData.frames.length;
-const delayFrame = -20;
+//const fLen = mockData.frames.length;
+const delayFrame = -2;
 const frameRate = 30;
 const minPoseConfidence = 0;
-// var time = Date.now();
-
-// function test(a){
-//     console.log(time);
-// }
 
 function Player() {
     const [curFrame, setCurFrame] = useState(-1);
     const [player, setPlayer] = useState();
     const [playing, setPlaying] = useState(false);
-    const [score,setScore] = useState(0.0);
+    const [score, setScore] = useState(0.0);
+    const [style, setStyle] = useState('red');
+    const [mockData, setMockData]= useState([]);
+    const [fLen, setFLen]= useState(0);
+
+    useEffect(()=>{
+        getHttp("/trainings/7/poses")
+        .then(res => {
+            setMockData(res.data.frames)
+            return res.data.frames.length
+        })
+        .then(r=>setFLen(r))
+    },[])
+
+    useEffect(() => {
+        if (0 <= score && score < 60) {
+            setStyle('red');
+        } else if (60 <= score && score < 88) {
+            setStyle('yellow');
+        } else {
+            setStyle('green');
+        }
+
+    }, [score]);
 
     async function estimate(curFrame, setState, a) {
         var frame = curFrame + delayFrame;
@@ -27,7 +46,7 @@ function Player() {
             try {
                 if (mockData.frames[frame].keyPoint.length === 17 && a[0].keypoints.length === 17) {
                     cosineSimilarity(mockData.frames[frame], a[0])
-                    .then(score=>setScore(score))
+                        .then(score => setScore(score))
                     console.log(score);
                 } else {
                     console.log("전신이 나와야합니다.");
@@ -56,14 +75,15 @@ function Player() {
 
     return (
         <div>
+            <div style={{display:"block"}}>
             <ReactPlayer
                 id="player"
-                url='https://mexusapi.codingnome.dev/streaming/training/1/video' playing controls
+                url='https://mexusapi.codingnome.dev/streaming/training/7/video' playing controls
                 width="50%"
                 height="100%"
                 onPlay={onPlay}
                 onPause={onPause}
-                progressInterval={1000/10}
+                progressInterval={1000 / 10}
                 onProgress={onProgress}
                 // eslint-disable-next-line
                 playing={playing}
@@ -72,7 +92,7 @@ function Player() {
                 id="player"
                 className="shape"
                 ref={p => setPlayer(p)}
-                url='https://mexusapi.codingnome.dev/streaming/training/1/shape' playing controls
+                url='https://mexusapi.codingnome.dev/streaming/training/7/shape' playing controls
                 width="50%"
                 // eslint-disable-next-line
                 playing={playing}
@@ -80,7 +100,7 @@ function Player() {
             />
             <PoseNet
                 className="cam"
-                style={{zIndex: "-1"}}
+                style={{ zIndex: "-1" }}
                 onEstimate={(a) => estimate(curFrame, setCurFrame, a)}
                 frameRate={frameRate}
                 flipHorizontal={true}
@@ -89,10 +109,14 @@ function Player() {
                 minPoseConfidence={minPoseConfidence}
                 curFrame={curFrame}
             />
-            {score}
+            </div>
+            <div style={{background: style,fontSize: "30px",width:"100%",display:"flex",justifyContent: "center"}}>
+                {score}
+            </div>
         </div>
+
+
     );
 }
-
 
 export default Player;
