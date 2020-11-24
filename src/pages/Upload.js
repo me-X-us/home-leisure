3fe32fb7e74a98dcbd228e39409c35de26dca3ff
Namkwangwoon <nkw096@naver.com>
2020-11-24 17:24:15 +0900

    feat: 영상 업로드

--- src/pages/Upload.js
+++ src/pages/Upload.js
@@ -5,18 +5,18 @@ import '../css/Upload.css';
 const Upload = (props) => {
 
     const [uploadAvailable, setUploadAvailable] = useState(false);
-    // const [video, setVideo] = useState('');
+    const [video, setVideo] = useState('');
     const [thumbnail, setThumbnail] = useState('');
     const [videoName, setVideoName] = useState("");
     const [videoExplain, setVideoExplain] = useState("");
 
     useEffect(() => {
-        if (thumbnail === "" || videoName === "" || videoExplain === "") {
+        if (video === '' || thumbnail === "" || videoName === "" || videoExplain === "") {
             setUploadAvailable(false);
         } else {
             setUploadAvailable(true);
         }
-    }, [thumbnail, videoName, videoExplain]);
+    }, [video, thumbnail, videoName, videoExplain]);
 
     // 영상(1)과 썸네일(2) 업로드 추후에 진행
 
@@ -25,15 +25,20 @@ const Upload = (props) => {
 
 
     const upload = async () => {
+        let tID;
         let uploadVideo = await postHttp('/trainings', {
             title: videoName,
             body: videoExplain
         }).then(response => {
-            let tID = response.data._links.self.href.slice(-1);
+            tID = response.data._links.self.href.slice(-1);
             const fd = new FormData();
             // console.log(fd)
             fd.append('image', thumbnail)
             postHttp('/thumbnail/'+tID, fd)
+        }).then(response => {
+            const fd = new FormData();
+            fd.append('video', video)
+            postHttp('/upload/trainings/'+tID, fd)
         }).catch(error => {
             console.log('error on component : ', error.response.data)
         })
@@ -43,13 +48,17 @@ const Upload = (props) => {
         }
     }
 
+    const UploadVideo = (e) => {
+        setVideo(e.target.files[0])
+    }
+
     const UploadThumbnail = (e) => {
         setThumbnail(e.target.files[0])
     }
     
     return (
         <div className='UploadWrapper'>
-            <input className='VideoUpload' type='file' accept='.mp4'/>  {/** 업로드할 영상을 선택해주세요 */}
+            <input className='VideoUpload' type='file' accept='.mp4' onChange={UploadVideo} />  {/** 업로드할 영상을 선택해주세요 */}
             <input className='ThumbNailUpload' type='file' accept='image/jpg,impge/png,image/jpeg,image/gif' onChange={UploadThumbnail}/>   {/** 대표사진을 선택해주세요. */}
             <br />
             <input className='UploadTitle' placeholder='영상 제목 입력' onChange={changeVideoName} />
