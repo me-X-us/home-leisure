c6853849785035bc83a4b29c1328908ac06d869a
Namkwangwoon <nkw096@naver.com>
2020-11-24 17:22:53 +0900

    feat: 좋아요 & 구독

--- src/components/VideoInfo.js
+++ src/components/VideoInfo.js
@@ -1,16 +1,53 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import '../css/VideoInfo.css'
+import { deleteHttp, postHttp, getUserId } from '../utils/authHttpWrapper';
 
 const VideoInfo = (props) => {
+    const [like, setLike] = useState(props.trainingInfo.likes)
+    const [subscribe, setSubscribe] = useState(props.trainingInfo.subscribe)
 
+    useEffect(() => {
+        setLike(props.trainingInfo.likes)
+    }, [props.trainingInfo.likes])
+
+    useEffect(() => {
+        setSubscribe(props.trainingInfo.subscribe)
+    }, [props.trainingInfo.subscribe])
+
+    const addLike = () => {
+        
+        if (props.trainingInfo.like === false) {
+            postHttp('/trainings/' + props.trainingId + '/like', {})
+            .then(()=>props.getTrainingInfo())
+            .then(()=>alert('좋아요 되었습니다.'))
+        } else {
+            deleteHttp('/trainings/' + props.trainingId + '/like')
+            .then(()=>props.getTrainingInfo())
+            .then(()=>alert('좋아요 취소되었습니다.'))
+        }
+    }
+
+    const ChangeSubscribe = async () => {
+        let userId = await getUserId()
+        
+        if (props.trainingInfo.subscribe === false) {
+            postHttp('/trainer/' + userId, {})
+            .then(()=>props.getTrainingInfo())
+            .then(()=>alert('구독 되었습니다.'))
+        } else {
+            deleteHttp('/trainer/' + userId)
+            .then(()=>props.getTrainingInfo())
+            .then(()=>alert('구독 취소되었습니다.'))
+        }
+    }
 
     return (
         <div className='VideoInfos'>
             <div className='NameLike'>
                 <text className='TrainingVideoName'>{props.trainingInfo.title}</text>
                 <div className='Like'>
-                    <button className='LikeButton' />
-                    <text>1000</text>
+                    <button className='LikeButton' onClick={addLike} />
+                    <text>{like}</text>
                 </div>
                 {/* <button className='InputButton'>+</button> */}
             </div>
@@ -21,7 +58,7 @@ const VideoInfo = (props) => {
             <div className='ChannelInfo'>
                 <img className='Profile' src="https://avatars2.githubusercontent.com/u/18184139?s=64&v=4" alt="" />
                 <text className='ChannelName'>{props.trainingInfo.trainer}</text>
-                <button className='SubscribeButton'>구독</button>
+                <button className='SubscribeButton' onClick={ChangeSubscribe}>{subscribe?'구독중':'구독'}</button>
             </div>
             <div className='VideoExplain'>
                 <text>{props.trainingInfo.body}</text>
